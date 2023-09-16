a66af6dd3b2904b187deffd38b28251e4cca4707
Erin Ramos <47257106+Erin-Ramos@users.noreply.github.com>
2023-09-16 11:27:49 -0400

    Project Setup

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+*/
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+let startIndex;
+let endIndex;
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+
+function showPage(list, page) {
+   startIndex = (page * items per page) - items per page;
+   endIndex = page * items per page;
+}
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+function addPagination {
+
+}
+
+
+// Call functions
+showPage(list, page);
+addPagination();
