cbaaa7e40405d040f792a78934df6da56768dca1
Erin Ramos <47257106+Erin-Ramos@users.noreply.github.com>
2023-09-23 21:55:57 -0400

    Work added

--- js/script.js
+++ js/script.js
@@ -9,17 +9,35 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-let startIndex;
-let endIndex;
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
 function showPage(list, page) {
-   startIndex = (page * items per page) - items per page;
-   endIndex = page * items per page;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+   let studentInfo = '';
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+
+   for (i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+         studentInfo += `
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src="${data[i].picture.large}" alt="Profile Picture">
+               <h3>${data[i].name.first} ${data[i].name.last}</h3>
+               <span class="email">${data[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">${data[i].registered.date}</span>
+            </div>
+         </li>
+         `;
+      }
+   }
+   studentList.insertAdjacentHTML('beforeend', studentInfo);
 }
 
 /*
@@ -27,11 +45,11 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
-function addPagination {
+function addPagination() {
 
 }
 
 
 // Call functions
-showPage(list, page);
+showPage(data, 1);
 addPagination();
